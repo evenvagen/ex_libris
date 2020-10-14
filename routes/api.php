ef741fd6b8000e3abb7c22439bf36d2ec07d2dfb
evenvagen <evenvagen@gmail.com>
2020-10-14 21:02:26 +0200

    api-route til users

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ApiControllers\BookController;
+use App\Http\Controllers\ApiControllers\UserControllerAPI;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('books', BookController::class);
+
+Route::get('users', [UserControllerAPI::class, 'index']);
