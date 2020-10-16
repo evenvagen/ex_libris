66b7268e1f33a698abf73c1db29e48956c289d57
evenvagen <evenvagen@gmail.com>
2020-10-16 09:26:06 +0200

    livewire binding(#2)

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -3,13 +3,17 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use phpDocumentor\Reflection\PseudoTypes\False_;
 
 class HelloWorld extends Component
 {
+
+    public $animal = 'Dog';
+    public $loud = false;
+    public $greeting = ['Hello'];
+
     public function render()
     {
-        return view('livewire.hello-world', [
-            'name' => 'Evan',
-        ]);
+        return view('livewire.hello-world');
     }
 }
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,7 +1,19 @@
 <div>
-    <p>
-        Hello {{$name}}
-    </p>
+
+    <input wire:model.debounce.1000ms="animal" type="text">
+    <input wire:model="loud" type="checkbox">
+
+    <select wire:model="greeting" multiple>
+
+        <option>Hello</option>
+        <option>Goodbye</option>
+        <option>Adios</option>
+
+    </select>
+
+    {{ implode(' + ', $greeting)}} {{$animal}}
+    @if($loud) !!! @endif
+
 </div>
 
 
