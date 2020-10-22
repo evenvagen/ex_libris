cb7131ad5b5bb188079c63ee6983887c35df88c5
evenvagen <evenvagen@gmail.com>
2020-10-22 11:01:42 +0200

    visual changes to make the seperate forms more alike

--- resources/views/book/create.blade.php
+++ resources/views/book/create.blade.php
@@ -6,25 +6,22 @@
 
 @section('content')
 
-<h1>Create books...</h1>
+<h1>Add books</h1>
 
 <form method="post" action="{{ route('books.store')}}">
     @csrf
     <div class="form-group">
-        <label for="title">Tittel:</label>
-        <input type="text" class="form-control" name="title"/>
+        <input type="text" class="form-control" name="title" placeholder="Tittel"/>
     </div>
 
     <div class="form-group">
-        <label for="author">Forfatter:</label>
-        <input type="text" class="form-control" name="author"/>
+        <input type="text" class="form-control" name="author" placeholder="Forfatter"/>
     </div>
 
     <div class="form-group">
-        <label for="description">Beskrivelse:</label>
-        <input type="text" class="form-control" name="description"/>
+        <input type="password" class="form-control" name="description" placeholder="Beskrivelse"/>
     </div>
-    <button type="submit" class="btn btn-primary-outline">Legg til bok</button>
+    <button type="submit" class="btn btn-primary">Legg til bok</button>
 </form>
 
 @endsection
--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -7,22 +7,19 @@
 @section('content')
 
 
-
+<h1>Create profile</h1>
 <form method="post" action="{{ route('users.store')}}">
     @csrf
     <div class="form-group">
-        <label for="name">Name</label>
         <input type="text" class="form-control" name="name" placeholder="Navn"/>
     </div>
 
 
     <div class="form-group">
-        <label for="email">Email address</label>
         <input type="email" class="form-control" name="email" placeholder="Email">
       </div>
 
     <div class="form-group">
-        <label for="password">Password:</label>
         <input type="text" class="form-control" name="password" placeholder="Password" />
     </div>
     <button type="submit" class="btn btn-primary">Legg til bruker</button>
