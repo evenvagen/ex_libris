c1b2f8cbd265af324e9a0ec11bf96d7408fcc615
evenvagen <evenvagen@gmail.com>
2020-10-22 13:17:56 +0200

    timezones

--- config/app.php
+++ config/app.php
@@ -123,6 +123,8 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
+    'timezone' => 'Europe/Oslo',
+
     /*
     |--------------------------------------------------------------------------
     | Autoloaded Service Providers
