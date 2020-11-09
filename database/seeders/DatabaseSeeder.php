93b7773315c250093c5f87e63813ead4b7e1141f
evenvagen <evenvagen@gmail.com>
2020-11-09 13:22:17 +0100

    organized blade files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(1)->create();
         \App\Models\Book::factory(5)->create();
-        // \App\Models\Rating::factory(15)->create();
     }
 }
